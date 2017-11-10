0736a35e0ce6703596b706c46d2a6824485453d5
David Veytsman <dveyts@yahoo.com>
2017-11-10 16:52:27 -0500

    Game functional

--- shooter.js
+++ shooter.js
@@ -2,12 +2,32 @@ var pause = false;
 var globalTimer = 0;
 var squareWidth = 40;
 var squareHeight = 40;
+var score = 0;
+var sprite = new Image();
+var cx = 0;
+var cy = 0;
+var sx = 0;
+var sy = 0;
+var swidth = 70;
+var sheight = 70;
+    sprite.src = "images/asteroid1.png";
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
 
   ctx.fillStyle = "white";
   var count = 0;
+//   function drawScore() {
+//     ctx.font = "16px Arial";
+//     ctx.fillStyle = "#0095DD";
+//     ctx.fillText("Score: "+score, 8, 20);
+// }
+//   function drawMisses(){
+//     ctx.fillText("you missed " + count + " stones", 300, 20);
+//   }
+//   function droppy(){
+//     return count;
+//   }
   function Square(x, y, w, h){
     this.x = x;
     this.y = y;
@@ -26,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
         count += 1;
 
         if(count >= 3){
-          // pause = true;
+          pause = true;
         }
       } else {
         if(!pause){
@@ -54,18 +74,24 @@ document.addEventListener("DOMContentLoaded", () => {
   let timer = 0;
   function fall(){
 
-    if ( globalTimer % 30 === 0) {
+
+
+
+    if ( globalTimer % 300 === 0) {
       if (count >=3){
+        // drawMisses();
         // pause = true;
       }
       rockArray.push(new Square(Math.random()* 490, 0, squareWidth, squareHeight));
     }
 
     ctx.clearRect(0, 0, 500, 500);
-
+    // drawScore();
+    // drawMisses();
     for (var j = 0; j < rockArray.length; j++) {
       // setTimeout(3000);
       rockArray[j].update();
+            ctx.drawImage(sprite, sx, sy, swidth, sheight, cx, cy, 60, 60);
       // console.log(rockArray);
     }
     globalTimer++;
@@ -84,6 +110,10 @@ document.addEventListener("DOMContentLoaded", () => {
     switch (e.key) {
       case 'r':
       rockArray = [];
+      rockArray.push(new Square(Math.random()* 490, -100, squareWidth, squareHeight));
+      pause = false;
+      count = 0;
+      score = 0;
         break;
       case ' ':
         pause = !pause;
@@ -93,18 +123,22 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
       }
   });
-  document.addEventListener("click", (e) => {
+  document.addEventListener("mousemove", (e) => {
+    if(!pause){
+
     var coords = getMousePos(canvas, e);
     for (var i = 0; i < rockArray.length; i++) {
       if((rockArray[i].y + squareHeight > coords.y) && (rockArray[i].y < coords.y)){
         if((rockArray[i].x + squareWidth > coords.x)&& (rockArray[i].x  < coords.x)){
           rockArray[i].y = Math.random() * -100 - squareHeight/2;
           rockArray[i].x = Math.random() * 480;
-          console.log(rockArray[i].y);
+          score++;
+          // alert(score);
 
         }
       }
     }
+  }
   });
   var then;
   var now;
@@ -118,15 +152,30 @@ document.addEventListener("DOMContentLoaded", () => {
     startTime = then;
     animate();
   }
+  // var sprite = new Image();
+  // var cx = 0;
+  // var cy = 0;
+  // var sx = 0;
+  // var sy = 0;
+  // var swidth = 80;
+  // var sheight = 80;
+  // sprite.onload = function(){
+  //   ctx.drawImage(sprite, sx, sy, swidth, sheight, cx, cy, 50, 50);
+  // };
   function animate(){
     requestAnimationFrame(animate);
     now = Date.now();
     elapsedTime = now - then;
+    // drawScore();
 
     if(elapsedTime > fpsInterval){
       then = now - (elapsedTime % fpsInterval);
       if(!pause){
         fall();
+        document.getElementById("counter").innerHTML = "lives " + (3 - count);
+        document.getElementById("score").innerHTML = "score " + (score);
+        // document.write('hi');
+        // counter.append(count);
       }
     }
   }
